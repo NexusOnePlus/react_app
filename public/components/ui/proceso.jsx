b4c10360001bc2c9feb60ad6b78b77fcbf56e366
Nexus <wv35788@gmail.com>
2024-05-18 16:18:35 -0500

    Talvez hay bugs, pero no lo se...

--- public/components/ui/proceso.jsx
+++ public/components/ui/proceso.jsx
@@ -8,7 +8,7 @@ const Tablitas = ({ matrix }) => {
       {matrix.map((row, rowIndex) => (
         <div className='matrix-table' key={rowIndex}>
           {row.map((cell, cellIndex) => (
-            <span className="cell" key={cellIndex}>{cell} </span>
+            <span className="cell" key={cellIndex}>{cell } </span>
           ))}
         </div>
       ))}
@@ -32,43 +32,69 @@ const operaciones = (nuevo, signos, matrix) => {
     nuevafila.push(colita)
     return nuevafila;
   });
-
-  let pivote, pivot;
-       for (let j = 0, i = 0, valor = Infinity; j < nuevoConIdentidad[i].length; j++) {
-        if (nuevoConIdentidad[i][j] < valor) {
-          valor = nuevoConIdentidad[i][j];
-          pivote = j;
+  let negativos = true;
+  while (negativos) {
+    let pivote, pivot;
+    //eligiendo columna pivote
+    for (let j = 0, i = 0, valor = Infinity; j < nuevoConIdentidad[i].length; j++) {
+      if (nuevoConIdentidad[i][j] < valor && nuevoConIdentidad[i][j] < 0) {
+        valor = nuevoConIdentidad[i][j];
+        pivote = j;
+      }
+    }
+    if (pivote == undefined) {
+      break
+    } else {
+      //eligiendo fila pivote
+      console.log("BUSCANDO columna")
+      console.log(nuevoConIdentidad)
+      for (let i = 1, j = pivote, valor = Infinity; i < nuevoConIdentidad.length; i++) {
+        console.log("ite", i, "i", j, "j", "valor actual", nuevoConIdentidad[i][j])
+        console.log("division", nuevoConIdentidad[i][nuevoConIdentidad[i].length - 1]/nuevoConIdentidad[i][j])
+        console.log("valor", valor)
+        console.log("valor", nuevoConIdentidad[i][nuevoConIdentidad[i].length - 1])
+        if ((nuevoConIdentidad[i][nuevoConIdentidad[i].length - 1] / nuevoConIdentidad[i][j]) < valor && nuevoConIdentidad[i][j] != 0) {
+          valor = (nuevoConIdentidad[i][nuevoConIdentidad[i].length - 1]/nuevoConIdentidad[i][j]);
+          pivot = i;
         }
-       }
-      //  console.log("pivote columna" , pivote);
-       for (let i = 1, j = pivote, valor = Infinity; i < nuevoConIdentidad.length; i++) {
-        // console.log("division", nuevoConIdentidad[i][nuevoConIdentidad[i].length - 1]/nuevoConIdentidad[i][j])
-        // console.log("ite", i, "i", j, "j", nuevoConIdentidad[i][j])
-            if (nuevoConIdentidad[i][nuevoConIdentidad[i].length - 1]/nuevoConIdentidad[i][j] < valor && nuevoConIdentidad[i][j] != 0) {
-              valor = nuevoConIdentidad[i][j];
-              pivot = i;
-            }           
-       }
-       if (pivot == undefined) {
-        pivot = 1;
-       }
-  pasos.push(structuredClone(nuevoConIdentidad));
-  let realpivot = [... nuevoConIdentidad[pivot]];
-  // console.log("antes", realpivot)
+      }
+      if (pivot != undefined) {
+        let realpivot = [...nuevoConIdentidad[pivot]];
+        // reduciendo a 1 la fila pivot
+        for (let i = 0; i < nuevoConIdentidad[pivot].length; i++) {
+          nuevoConIdentidad[pivot][i] = (nuevoConIdentidad[pivot][i] / realpivot[pivote]);
+          if (!Number.isInteger(nuevoConIdentidad[pivot][i])) {
+            nuevoConIdentidad[pivot][i] = Math.floor(nuevoConIdentidad[pivot][i] * 100) / 100;
+          }
+        }
+        pasos.push(structuredClone(nuevoConIdentidad));
 
-  // console.log("tamaño", nuevoConIdentidad[pivot].length)
-  for (let i = 0; i < nuevoConIdentidad[pivot].length; i++) {
-    nuevoConIdentidad[pivot][i] =  nuevoConIdentidad[pivot][i]/realpivot[pivote];
+        // restaurando cambios a la copia de fila pivot
+        realpivot = [...nuevoConIdentidad[pivot]];
+        // transformando a 0 la fila z
+        let sumaresta = nuevoConIdentidad[0][pivote] * -1;
+        for (let i = 0; i < nuevoConIdentidad[0].length; i++) {
+          nuevoConIdentidad[0][i] = nuevoConIdentidad[0][i] + (realpivot[i] * (sumaresta));
+        }
+        pasos.push(structuredClone(nuevoConIdentidad));
+        // limpiando la columna pivote
+        for (let i = 1; i < nuevoConIdentidad.length; i++) {
+          let a = nuevoConIdentidad[i][pivote];
+          if (i != pivot && nuevoConIdentidad[i][pivote] != 0) {
+            for (let j = 0; j < nuevoConIdentidad[i].length; j++) {
+              // console.log("resta", nuevoConIdentidad[i][j], "xd", realpivot[j] * a)
+              nuevoConIdentidad[i][j] = nuevoConIdentidad[i][j] - (realpivot[j] * a);
+            }
+          }
+        }
+        pasos.push(structuredClone(nuevoConIdentidad));
+      }else {
+        negativos = false
+      }
+      // pasos.push(structuredClone(nuevoConIdentidad));
+    } 
   }
-  pasos.push(structuredClone(nuevoConIdentidad));
-  // console.log("despues", nuevoConIdentidad[pivot])
-  realpivot = [... nuevoConIdentidad[pivot]];
-  let sumaresta = nuevoConIdentidad[0][pivote]*-1;
-  for (let i = 0; i < nuevoConIdentidad[0].length; i++) {
-    nuevoConIdentidad[0][i] =  nuevoConIdentidad[0][i]+realpivot[i]*(sumaresta);
-  }
-  pasos.push(structuredClone(nuevoConIdentidad));
-  // console.log("despues si", realpivot)
+
   pasos.push(matrix);
   return pasos;
 };
